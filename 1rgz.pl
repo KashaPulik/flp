4b74efbebbb001740ff6fb1e6c725aea830a7e5b
Kulik Pavel <pashakulik58@gmail.com>
2024-11-11 19:12:28 +0700

    Небольшие правки в отчёте

--- 1rgz.pl
+++ 1rgz.pl
@@ -1,13 +1,13 @@
 cyclic_shift(List, N, ShiftedList) :-
     length(List, Len),
-    N1 is N mod Len,     
+    N1 is N mod Len,
     shift_left(List, N1, ShiftedList).
 
-shift_left(List, 0, List).    
+shift_left(List, 0, List).
 shift_left([H|T], N, Shifted) :-
     N > 0,
-    append(T, [H], Temp),     
-    N1 is N - 1,              
+    append(T, [H], Temp),
+    N1 is N - 1,
     shift_left(Temp, N1, Shifted). 
 
 start :-
--- 2rgz.pl
+++ 2rgz.pl
@@ -1,20 +1,20 @@
 reverse_words_in_file(InputFile, OutputFile) :-
-    open(InputFile, read, InStream),               
-    open(OutputFile, write, OutStream),            
-    process_lines(InStream, OutStream),            
-    close(InStream),                               
-    close(OutStream).                              
+    open(InputFile, read, InStream),
+    open(OutputFile, write, OutStream),
+    process_lines(InStream, OutStream),
+    close(InStream),
+    close(OutStream).
 
 process_lines(InStream, OutStream) :-
-    read_line_to_string(InStream, Line),           
-    (   Line \= end_of_file                        
-    ->  reverse_line_words(Line, ReversedLine),    
-        writeln(OutStream, ReversedLine),          
-        process_lines(InStream, OutStream)         
-    ;   true                                       
+    read_line_to_string(InStream, Line),
+    (   Line \= end_of_file
+    ->  reverse_line_words(Line, ReversedLine),
+        writeln(OutStream, ReversedLine),
+        process_lines(InStream, OutStream)
+    ;   true
     ).
 
 reverse_line_words(Line, ReversedLine) :-
-    split_string(Line, " ", "", Words),            
-    reverse(Words, ReversedWords),                 
+    split_string(Line, " ", "", Words),
+    reverse(Words, ReversedWords),
     atomic_list_concat(ReversedWords, " ", ReversedLine).
